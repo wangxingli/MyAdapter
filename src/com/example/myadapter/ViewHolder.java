5ffb238b28f1cfe2fca14438baf8278e992f4881
wang <wang@DESKTOP-1TSNJ8S>
2016-08-21 23:35:26 +0800

    第一次提交

---
+++ src/com/example/myadapter/ViewHolder.java
@@ -0,0 +1,8 @@
+package com.example.myadapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class ViewHolder {
+	public TextView tv = null;
+	public CheckBox cb = null;
+}
